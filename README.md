9ff943ba956647537dcbefc3946f6556659cc5b6
pineshen <pineshen@gmail.com>
2019-06-05 23:41:08 +0800

    1st version, english

---
+++ README.md
@@ -0,0 +1,14 @@
+# [主页]
+- 官网地址:
+- 使用 [阿里 Ant Design Landing](https://landing.ant.design/index-cn) 编辑开发
+- 基于脚手架 [umi](https://landing.ant.design/docs/use/umi) 创建
+
+![WX20190325-172718@2x.png](https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg)
+
+### 本地开发运行
+- npm install --save umi
+- npm i
+- umi dev
+- 默认端口8000
+### 打包部署
+- umi build
---
+++ src/Home/data.source.js
@@ -0,0 +1,606 @@
+import React from 'react';
+export const Nav00DataSource = {
+  wrapper: { className: 'header0 home-page-wrapper' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header0-logo',
+    children: 'https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg',
+  },
+  Menu: {
+    className: 'header0-menu',
+    children: [
+      { name: 'item0', a: { children: 'Homepage', href: 'http://118.25.182.62/index.html' } },
+      {
+        name: 'item1',
+        a: {
+          children: 'Capability',
+          href: 'http://118.25.182.62/index.html',
+          target: '_black',
+        },
+      },
+      {
+        name: 'item2',
+        a: {
+          children: 'Project',
+          href: 'https://118.25.182.62/index.html',
+          target: '_black',
+        },
+      },
+      {
+        name: 'item3',
+        a: {
+          children: 'Business Planning',
+          href: 'http://118.25.182.62/index.html',
+          target: '_black',
+        },
+      },
+      {
+        name: 'item4',
+        a: {
+          children: 'Contact',
+          href: 'http://wpa.qq.com/msgrd?v=3&uin=631370562&site=qq&menu=yes',
+          target: '_black',
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header0-mobile-menu' },
+};
+export const Banner00DataSource = {
+  wrapper: { className: 'banner0' },
+  textWrapper: { className: 'banner0-text-wrapper' },
+  title: {
+    className: 'banner0-title jtnstd5q52-editor_css',
+    children: 'https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png',
+  },
+  content: {
+    className: 'banner0-content',
+    children: (
+      <span>
+        <p>Embedded development, image processing, IoT</p>
+      </span>
+    ),
+  },
+  button: {
+    className: 'banner0-button jtntac62itf-editor_css',
+    href: "http://118.25.182.62/index.html",
+    children: (
+      <span>
+        <p>go</p>
+      </span>
+    )
+  },
+};
+export const Content00DataSource = {
+  wrapper: { className: 'home-page-wrapper content0-wrapper' },
+  page: { className: 'home-page content0' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>Capability</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  block: {
+    className: 'block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
+          },
+          title: {
+            className: 'content0-title',
+            children: (
+              <span>
+                <p>Embedded development</p>
+              </span>
+            ),
+          },
+          content: {
+            children: (
+              <span>
+                <p>IT device, Vehicle equipment, Network device, GUI, HW Development board</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
+          },
+          title: {
+            className: 'content0-title',
+            children: (
+              <span>
+                <p>Application development</p>
+              </span>
+            ),
+          },
+          content: {
+            children: (
+              <span>
+                <p>Windows Application, Linux Application, Mobile App and device driver</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
+          },
+          title: {
+            className: 'content0-title',
+            children: (
+              <span>
+                <p>IoT development</p>
+              </span>
+            ),
+          },
+          content: {
+            children: (
+              <span>
+                <p>IoT system, Web Backend, Android App, Wechart App</p>
+              </span>
+            ),
+          },
+        },
+      },
+    ],
+  },
+};
+export const Content30DataSource = {
+  wrapper: { className: 'home-page-wrapper content3-wrapper' },
+  page: { className: 'home-page content3' },
+  OverPack: { playScale: 0.3 },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>Project</p>
+          </span>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        className: 'title-content',
+        children: (
+          <span>
+            <p>Computer peripheral, Image Process Software, Network Software</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  block: {
+    className: 'content3-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ScHBSdwpTkAHZkJ.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>Scanner</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>CCD, CIS, slim, high speed, film, Currency Identification, Recognition Algorithms of US Dollar, Euro, Malaysian Dollar, Costa Rican Dollar and so on</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/NKBELAOuuKbofDD.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>Printer</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>
+                  Printer firmware, protocol stack, print protocol
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xMSBjgxBhKfyMWX.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>Application</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>
+                  Image process, Image algorithm
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block3',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/MNdlBNhmDBLuzqp.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>IP Camera</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>Linux, NFS, buildroot, onvif protocol</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block4',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/UsUmoBRyLvkIQeO.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>Industrial Automation</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>
+                  AGV firmware, AGV Distributed Navigation System, modbus, bacnet, SCADA, Protocol Conversion
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block5',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ipwaQLBLflRfUrg.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>IOT</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>
+                  Secondary Development of WIFI Module, MQTT broker, Wechart APP and backend, APP
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+    ],
+  },
+};
+export const Content10DataSource = {
+  wrapper: { className: 'home-page-wrapper content1-wrapper' },
+  OverPack: { className: 'home-page content1', playScale: 0.3 },
+  imgWrapper: { className: 'content1-img', md: 10, xs: 24 },
+  img: {
+    children: 'https://zos.alipayobjects.com/rmsportal/nLzbeGQLPyBJoli.png',
+  },
+  textWrapper: { className: 'content1-text', md: 14, xs: 24 },
+  title: {
+    className: 'content1-title',
+    children: (
+      <span>
+        <p>Business Direction</p>
+      </span>
+    ),
+  },
+  content: {
+    className: 'content1-content',
+    children: (
+      <span>
+        <p>Development Board, IOT, Web</p>
+      </span>
+    ),
+  },
+};
+export const Content110DataSource = {
+  OverPack: {
+    className: 'home-page-wrapper content11-wrapper',
+    playScale: 0.3,
+  },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'image',
+        children:
+          'https://gw.alipayobjects.com/zos/rmsportal/PiqyziYmvbgAudYfhuBr.svg',
+        className: 'title-image',
+      },
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>Customers</p>
+          </span>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        children: (
+          <span>
+            <p>Epson</p>
+          </span>
+        ),
+        className: 'title-content',
+      },
+      {
+        name: 'content2',
+        children: (
+          <span>
+            <p>Ti</p>
+          </span>
+        ),
+        className: 'title-content',
+      },
+      {
+        name: 'content3',
+        children: (
+          <span>
+            <p>3M</p>
+          </span>
+        ),
+        className: 'title-content',
+      },
+    ],
+  },
+  button: {
+    className: '',
+    children: {
+      a: {
+        className: 'button',
+        href: 'http://wpa.qq.com/msgrd?v=3&uin=631370562&site=qq&menu=yes',
+        children: (
+          <span>
+            <p>Contact</p>
+          </span>
+        ),
+        target: '_blank',
+      },
+    },
+  },
+};
+export const Footer10DataSource = {
+  wrapper: { className: 'home-page-wrapper footer1-wrapper' },
+  OverPack: { className: 'footer1', playScale: 0.2 },
+  block: {
+    className: 'home-page',
+    children: [
+      {
+        name: 'block0',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          className: 'logo',
+          children: 'https://zos.alipayobjects.com/rmsportal/qqaimmXZVSwAhpL.svg',
+        },
+        content: {
+          className: 'slogan',
+          children: (
+            <span>
+              <p>
+                Embedded products,automation equipment,IoT Application
+              </p>
+            </span>
+          ),
+        },
+      },
+      {
+        name: 'block1',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: { children: 'Product' },
+        content: {
+          children: (
+            <span>
+              <p>
+                <a href="http://118.25.182.62/STM32H750">Development board</a>
+              </p>
+              <br/>
+              <p>
+                <a href="http://118.25.182.62/scada">SCADA </a>
+              </p>
+              <br/>
+              <p>
+                <a href="http://118.25.182.62/iot">IoT</a>
+              </p>
+              <br/>
+              <p>
+                <a href="http://wpa.qq.com/msgrd?v=3&uin=631370562&site=qq&menu=yes" target="_blank">Customization Development</a>
+              </p>
+              <br/>
+            </span>
+          ),
+        },
+      },
+      {
+        name: 'block2',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: { children: 'About' },
+        content: {
+          children: (
+            <span>
+              <p>
+                <a href="http://wpa.qq.com/msgrd?v=3&uin=631370562&site=qq&menu=yes" target="_blank">Contact</a>
+              </p>
+              <br/>
+            </span>
+          ),
+        },
+      },
+      {
+        name: 'block3',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: { children: 'Resource' },
+        content: {
+          children: (
+            <span>
+              <p>
+                <a href="https://github.com/pineshen/homepage" target="_blank">Source code of this site</a>
+              </p>
+              <br/>
+              <p>
+                <a href="https://www.iviewui.com" target="_blank">iView</a>
+              </p>
+              <br/>
+              <p>
+                <a href="https://github.com/iview/iview-admin" target="_blank">iView Admin</a>
+              </p>
+              <br/>
+            </span>
+          ),
+        },
+      },
+    ],
+  },
+  copyrightWrapper: { className: 'copyright-wrapper' },
+  copyrightPage: { className: 'home-page' },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <p>
+          ©2019 - Base on Ali <a href="https://landing.ant.design/index-cn">Ant Design Landing</a> technology <br />
+        </p>
+      </span>
+    ),
+  },
+};
